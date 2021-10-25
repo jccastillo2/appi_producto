93321dd0ed3e7994d29ed6e96445f9ce617e384d
Jean Castillo <jccastillo2@sudamericano.edu.ec>
2021-10-25 10:43:53 -0500

    first commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Productos (
+  id serial,
+  description VARCHAR(45) NOT NULL,
+  details VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+  );
---
+++ src/test/kotlin/com/Jeanky/tienda/TiendaApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.Jeanky.tienda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
